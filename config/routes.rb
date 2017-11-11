7f42856f34486b82c4235d1ef82fd80732067224
Vijay Kumar Agrawal <vijay@bigbinary.com>
2017-11-11 17:22:38 -0500

    Added home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resource :home, only: [:show]
+
   resources :tickets
   resources :users
   resources :organizations
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#show'
 end
